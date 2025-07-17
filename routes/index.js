85c9c8cd3286d8142f63f656f2a75823bcb235d3
Minato0256 <153624974+Minato0256@users.noreply.github.com>
2025-07-17 05:41:25 +0000

    手引き完了、削除機能追加、todoリストに具体的な内容と期限も入力できるように

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,49 @@ const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
   const isAuth = req.isAuthenticated();
-  knex("tasks")
-    .select("*")
-    .then(function (results) {
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
-        isAuth: isAuth,
+  if (isAuth) {
+    const userId = req.user.id;
+    knex("tasks")
+      .select("*")
+      .where({user_id: userId})
+      .then(function (results) {
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+          isAuth: isAuth,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+          isAuth: isAuth,
+          todos: [],
+          errorMessage: [err.sqlMessage],
+        });
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
-        isAuth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
+  } else {
+    res.render('index', {
+      title: 'ToDo App',
+      isAuth: isAuth,
+      todos: [],
     });
+  }
 });
 
 router.post('/', function (req, res, next) {
   const isAuth = req.isAuthenticated();
   const userId = req.user.id;
   const todo = req.body.add;
+  const detail = req.body.detail;
+  const due = req.body.due;
   knex("tasks")
-    .insert({user_id: userId, content: todo})
+    .insert({
+      user_id: userId,
+      content: todo,
+      detail: detail,
+      due: due
+    })
     .then(function () {
       res.redirect('/')
     })
@@ -37,6 +55,32 @@ router.post('/', function (req, res, next) {
       res.render('index', {
         title: 'ToDo App',
         isAuth: isAuth,
+        todos: [],
+        errorMessage: [err.sqlMessage],
+      });
+    });
+});
+
+// タスク削除用ルート
+router.post('/delete/:id', function (req, res, next) {
+  const isAuth = req.isAuthenticated();
+  if (!isAuth) {
+    return res.redirect('/signin');
+  }
+  const userId = req.user.id;
+  const taskId = req.params.id;
+  knex("tasks")
+    .where({ id: taskId, user_id: userId })
+    .del()
+    .then(function () {
+      res.redirect('/');
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
+        todos: [],
         errorMessage: [err.sqlMessage],
       });
     });
